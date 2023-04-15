7f3ee952800803ad7d497fbf44cebbace85f9428
Ricardo Dias <ricardodias.dev@gmail.com>
2023-04-15 09:41:03 -0300

    Corringindo o hydration error

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import { Nunito } from "next/font/google";
 import "./globals.css";
 import Navbar from "./api/components/navbar/Navbar";
+import ClientOnly from "./api/components/ClientOnly";
 
 export const metadata = {
   title: "Airbnb",
@@ -19,7 +20,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
-        <Navbar/>
+        <ClientOnly>
+          <Navbar />
+        </ClientOnly>
         {children}
       </body>
     </html>
