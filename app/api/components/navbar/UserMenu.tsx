e669fc98e2d715f91df6c0df97e034c3658de460
Ricardo Dias <ricardodias.dev@gmail.com>
2023-04-15 22:05:37 -0300

    Instalando bibliotecas e fazendo implementações

--- app/api/components/navbar/UserMenu.tsx
+++ app/api/components/navbar/UserMenu.tsx
@@ -3,8 +3,10 @@ import { useCallback, useState } from "react";
 import { AiOutlineMenu } from "react-icons/ai";
 import Avatar from "../Avatar";
 import MenuItem from "./MenuItem";
+import useRegisterModal from "../../hooks/useRegisterModal";
 
 const UserMenu = () => {
+  const registerModal = useRegisterModal()
   const [isOpen, setIsOpen] = useState(false);
   const toggleOpen = useCallback(() => {
     setIsOpen((value) => !value);
@@ -72,7 +74,7 @@ const UserMenu = () => {
           <div className="flex flex-col cursor-pointer">
             <>
               <MenuItem onClick={() => {}} label="Login" />
-              <MenuItem onClick={() => {}} label="Sign up" />
+              <MenuItem onClick={registerModal.onOpen} label="Sign up" />
             </>
           </div>
         </div>
