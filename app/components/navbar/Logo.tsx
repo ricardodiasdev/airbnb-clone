475296738b7abacf06620a35435f37df704e46d8
Ricardo Dias <ricardodias.dev@gmail.com>
2023-04-18 11:12:16 -0300

    Implementando Categories na Navbar e implementando onClick no Logo

--- app/components/navbar/Logo.tsx
+++ app/components/navbar/Logo.tsx
@@ -3,9 +3,10 @@ import Image from "next/image";
 import { useRouter } from "next/navigation";
 
 const Logo = () => {
-  const reouter = useRouter();
+  const router = useRouter();
   return (
     <Image
+      onClick={() => router.push("/")}
       alt="Logo"
       className="hidden md:block cursor-pointer"
       height="100"
