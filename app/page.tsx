15f1d5cffd5276b78868dba118839f0e01894ec7
Ricardo Dias <ricardodias.dev@gmail.com>
2023-04-21 19:24:53 -0300

    Implementando EmptyState no layout e page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,35 @@
+import ClientOnly from "./components/ClientOnly";
+import Container from "./components/Container";
+import EmptyState from "./components/EmptyState";
+
 export default function Home() {
+  const isEmpty = true;
+  if (isEmpty) {
+    return (
+      <ClientOnly>
+        <EmptyState showReset/>
+      </ClientOnly>
+    );
+  }
   return (
-    <h1 className="text-4xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <ClientOnly>
+      <Container>
+        <div
+          className="
+          pt-24
+          grid
+          grid-cols-1
+          sm:grid-cols-2
+          md:grid-cols-3
+          lg:grid-cols-4
+          xl:grid-cols-5
+          2xl:grid-cols-6
+          gap-8
+        "
+        >
+          <div>My future listing</div>
+        </div>
+      </Container>
+    </ClientOnly>
+  );
 }
