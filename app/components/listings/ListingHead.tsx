de28b35229ac3ec0e580761531420c9e0c5c9393
Ricardo Dias <ricardodias.dev@gmail.com>
2023-04-25 21:12:16 -0300

    Iniciando o componente ListingHead

---
+++ app/components/listings/ListingHead.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import useCountries from "@/app/hooks/useCountries";
+import { SafeUser } from "@/app/types";
+import Heading from "../Heading";
+
+interface ListingHeadProps {
+  title: string;
+  locationValue: string;
+  imageSrc: string;
+  id: string;
+  currentUser?: SafeUser | null;
+}
+const ListingHead: React.FC<ListingHeadProps> = ({
+  title,
+  locationValue,
+  imageSrc,
+  id,
+  currentUser,
+}) => {
+  const { getByValue } = useCountries();
+  const location = getByValue(locationValue);
+
+  return (
+    <>
+      <Heading
+        title={title}
+        subtitle={`${location?.region}, ${location?.label}`}
+      />
+    </>
+  );
+};
+
+export default ListingHead;
