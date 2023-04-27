6c65bc07122b2f62bb0577381e47547741c99d39
Ricardo Dias <ricardodias.dev@gmail.com>
2023-04-26 23:58:18 -0300

    Acrescentando informações no ListingClient

--- app/components/listings/ListingHead.tsx
+++ app/components/listings/ListingHead.tsx
@@ -1,8 +1,9 @@
-"use client";
 
 import useCountries from "@/app/hooks/useCountries";
 import { SafeUser } from "@/app/types";
+import Image from "next/image";
 import Heading from "../Heading";
+import HeartButton from "../HeartButton";
 
 interface ListingHeadProps {
   title: string;
@@ -27,6 +28,28 @@ const ListingHead: React.FC<ListingHeadProps> = ({
         title={title}
         subtitle={`${location?.region}, ${location?.label}`}
       />
+      <div
+        className="
+          w-full
+          h-[60vh]
+          overflow-hidden
+          rounded-xl
+          relative
+        "
+      >
+        <Image
+          alt="Image"
+          src={imageSrc}
+          fill
+          className="object-cover w-full"
+        />
+        <div className="absolute top-5 right-5">
+          <HeartButton
+            listingId={id}
+            currentUser={currentUser}
+          />
+        </div>
+      </div>
     </>
   );
 };
--- app/listings/[listingId]/ListingClient.tsx
+++ app/listings/[listingId]/ListingClient.tsx
@@ -1,3 +1,4 @@
+"use client"
 import Container from "@/app/components/Container";
 import ListingHead from "@/app/components/listings/ListingHead";
 import { categories } from "@/app/components/navbar/Categories";
@@ -18,25 +19,21 @@ const ListingClient: React.FC<ListingClientProps> = ({
   reservations = [],
   currentUser,
 }) => {
-
-    const category = useMemo(() => {
-        return categories.find((items) => 
-         items.label === listing.category);
-     }, [listing.category]);
-   
+  const category = useMemo(() => {
+    return categories.find((items) => items.label === listing.category);
+  }, [listing.category]);
 
   return (
     <Container>
       <div className="max-w-screen-lg mx-auto">
         <div className="flex flex-col gap-6">
-            <ListingHead
-              title={listing.title}
-              imageSrc={listing.imageSrc}
-              locationValue={listing.locationValue}
-              id={listing.id}
-              currentUser={currentUser}             
-            />
-
+          <ListingHead
+            title={listing.title}
+            imageSrc={listing.imageSrc}
+            locationValue={listing.locationValue}
+            id={listing.id}
+            currentUser={currentUser}
+          />
         </div>
       </div>
     </Container>
