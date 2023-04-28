f982e08fd6e3aa8f14866913446a70e635971580
Ricardo Dias <ricardodias.dev@gmail.com>
2023-04-27 23:17:37 -0300

    Iniciando o componente ListingCategory

---
+++ app/components/listings/ListingCategory.tsx
@@ -0,0 +1,22 @@
+"use client"
+
+import { IconType } from "react-icons";
+
+interface ListingCategoryProps{
+    icon: IconType
+    label: string
+    description: string
+}
+const ListingCategory: React.FC<ListingCategoryProps> = ({
+    icon,
+    label,
+    description
+}) => {
+    return (
+        <div>
+            Enter
+        </div>
+    );
+}
+
+export default ListingCategory;
