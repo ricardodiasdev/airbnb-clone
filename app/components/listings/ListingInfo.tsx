daa2e5b8797dc52fbb159af0bf76803fe7ab260b
Ricardo Dias <ricardodias.dev@gmail.com>
2023-04-28 18:27:11 -0300

    Acrescentando informações no ListingInfo

--- app/components/listings/ListingInfo.tsx
+++ app/components/listings/ListingInfo.tsx
@@ -1,10 +1,13 @@
 "use client";
 
+import { IconType } from "react-icons";
+import dynamic from "next/dynamic";
 import useCountries from "@/app/hooks/useCountries";
 import { SafeUser } from "@/app/types";
-import { IconType } from "react-icons";
 import Avatar from "../Avatar";
 import ListingCategory from "./ListingCategory";
+
+const Map = dynamic(() => import("../Map"), { ssr: false });
 
 interface ListingInfoProps {
   user: SafeUser;
@@ -49,7 +52,7 @@ const ListingInfo: React.FC<ListingInfoProps> = ({
           <div>Hosted by {user?.name}</div>
           <Avatar src={user?.image} />
         </div>
-        <div 
+        <div
           className="
             flex
             flex-row
@@ -59,26 +62,23 @@ const ListingInfo: React.FC<ListingInfoProps> = ({
             text-neutral-500
           "
         >
-            <div>
-                {guestCount} guests
-            </div>
-            <div>
-                {roomCount} rooms
-            </div>
-            <div>
-                {bathroomCount} bathrooms
-            </div>
+          <div>{guestCount} guests</div>
+          <div>{roomCount} rooms</div>
+          <div>{bathroomCount} bathrooms</div>
         </div>
       </div>
-      <hr/>
+      <hr />
       {category && (
-        <ListingCategory 
-            icon={category.icon}
-            label={category.label}
-            description={category.description}
+        <ListingCategory
+          icon={category.icon}
+          label={category.label}
+          description={category.description}
         />
-
-      )} 
+      )}
+      <hr />
+      <div className="text-lg font-light text-neutral-500">{description}- </div>
+      <hr />
+      <Map center={coordinates} />
     </div>
   );
 };
